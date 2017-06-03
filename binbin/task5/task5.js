86c9b60e8ef7b1aa479fa4b11a558d7ae45b988a
mountain410 <502597986@qq.com>
2017-06-03 23:31:37 +0800

    'task5-fix2'

--- binbin/task5/task5.js
+++ binbin/task5/task5.js
@@ -1,5 +1,6 @@
 var data = new Array();
 var dataList = '';
+var timer;
 var op = document.getElementById('op');
 var btns = op.getElementsByTagName('button');
 var input = document.getElementById('input');
@@ -9,7 +10,7 @@ var content = document.getElementById('content');
 btns[0].onclick = function(){
 	if (numCheck()) {
 		data.unshift(input.value);
-		render();
+		render(data);
 	}
 	input.value = '';
 	input.focus();
@@ -17,7 +18,7 @@ btns[0].onclick = function(){
 btns[1].onclick = function(){
 	if (numCheck()) {
 		data.push(input.value);
-		render();
+		render(data);
 	}
 	input.value = '';
 	input.focus();
@@ -34,7 +35,7 @@ btns[2].onclick = function(){
 	var conf = confirm("确定删除左侧元素" + data[0] + "?");
 	if(conf) {
 		data.shift();
-		render();
+		render(data);
 	} 
 }
 btns[3].onclick = function(){
@@ -47,7 +48,7 @@ btns[3].onclick = function(){
 	var conf = confirm("确定删除右侧元素" + data[data.length-1] + "?");
 	if(conf) {
 		data.pop();
-		render();
+		render(data);
 	} 
 }
 
@@ -56,7 +57,48 @@ content.addEventListener("click",function(e){
 	content.removeChild(e.target);
 },false)
 
+//可视化排序绑定点击事件
+var bubble = document.getElementById('btn-bubble');
+var insert = document.getElementById('btn-insertion');
+var select = document.getElementById('btn-selection');
 
+bubble.onclick = function(){
+	viewSort(bubbleSort,bubbleData);
+}
+insert.onclick = function(){
+	viewSort(insertionSort,insertData);
+}
+select.onclick = function(){
+	viewSort(selectionSort,selectionData);
+}
+
+//重置按钮
+var reset = document.getElementById('btn-reset');
+reset.onclick = function(){
+	if (timer) {
+		clearInterval(timer)
+	}
+	render(data);
+}
+
+//非可视化直接排序
+var binaryInsert = document.getElementById('btn-binaryInsert');
+var quick = document.getElementById('btn-quick');
+var btn_merge = document.getElementById('btn-merge');
+
+binaryInsert.onclick = function(){
+	var arr = data.slice(0);
+	binaryInsertionSort(arr);
+	render(arr);
+}
+quick.onclick = function(){
+	var arr = data.slice(0);
+	render(quickSort(arr));
+}
+btn_merge.onclick = function(){
+	var arr = data.slice(0);
+	render(mergeSort(arr));
+}
 
 //检查输入框是否为空和数字
 function numCheck(){
@@ -77,12 +119,12 @@ function numCheck(){
 }
 
 //渲染操作后的队列
-function render(){
+function render(dataArray){
 	content.innerHTML = '';
-	for (var i = 0; i < data.length; i++) {
+	for (var i = 0; i < dataArray.length; i++) {
 		var li = document.createElement('li');
-		li.style.height = data[i]*2 + "px";
-		li.innerHTML = data[i];
+		li.style.height = dataArray[i]*2 + "px";
+		li.innerHTML = dataArray[i];
 		content.appendChild(li);
 	}
 }
@@ -97,13 +139,14 @@ function bubbleSort(arr){
 				var temp = arr[j+1];
 				arr[j+1] = arr[j];
 				arr[j] = temp;
+				/*
+				 数组是引用类型不能直接赋值，需要复制一个。也可以写成这样：
+				 bubbleData.push(JSON.parse(JSON.stringify(arr)))
+				*/
+				bubbleData.push(arr.slice(0));		
 			}
-			bubbleData.push(arr);
 		}
-		bubbleData.push(arr);
 	}
-	bubbleData.push(arr);
-	return arr;
 }
 
 //选择排序
@@ -121,8 +164,8 @@ function selectionSort(arr){
 		temp = arr[i];
 		arr[i] = arr[minIndex];
 		arr[minIndex] = temp;
+		selectionData.push(arr.slice(0));
 	}
-	return arr;
 }
 
 //插入排序
@@ -134,9 +177,87 @@ function insertionSort(arr){
 		while (j >= 0 && arr[j] > key) {
 			arr[j+1] = arr[j];
 			j--;
+			insertData.push(arr.slice(0));
 		}
 		arr[j+1] = key;
+		insertData.push(arr.slice(0));
 	}
-	return arr;
 }
 
+//二分插入排序
+function binaryInsertionSort(arr) {
+  for (var i = 1; i < arr.length; i++) {
+      var key = arr[i], left = 0, right = i - 1;
+      while (left <= right) {
+          var middle = parseInt((left + right) / 2);
+          if (key < arr[middle]) {
+              right = middle - 1;
+          } else {
+              left = middle + 1;
+          }
+      }
+      for (var j = i - 1; j >= left; j--) {
+          arr[j + 1] = arr[j];
+      }
+      arr[left] = key;
+  }
+  return arr;
+}
+
+//快速排序（非in place）
+function quickSort(arr){
+	if (arr.length <= 1) {return arr;}
+	var pivotIndex = Math.floor(arr.length / 2);
+	var pivot = arr.splice(pivotIndex,1)[0];
+	var left = [];
+	var right = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] < pivot) {
+			left.push(arr[i]);
+		}else{
+			right.push(arr[i]);
+		}
+	}
+	return quickSort(left).concat([pivot],quickSort(right));
+}
+
+//归并排序
+function mergeSort(arr) {  //采用自上而下的递归方法
+  if(arr.length <= 1) {
+    return arr;
+  }
+  var middle = Math.floor(arr.length / 2),
+    left = arr.slice(0, middle),
+    right = arr.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+}
+function merge(left, right){
+  var result = [];
+  while (left.length>0 && right.length>0) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  while (left.length){
+  	result.push(left.shift());
+  }
+  while (right.length){
+  	result.push(right.shift());
+  }
+  return result;
+}
+
+//可视化显示排序
+function viewSort(sortAlgorithm,sortArray){
+	var arr = data.slice(0); //复制数组data
+	sortAlgorithm(arr);
+	timer = setInterval(function(){
+		if (sortArray.length > 0) {
+			render(sortArray.shift());
+		}else{
+			clearInterval(timer);
+		}
+	},500)
+}
