90c2fdcff1e942ea060608c4678bd8b485aa372f
mountain410 <502597986@qq.com>
2017-07-24 13:19:42 +0800

    '添加注释和fix'

--- yaoyao/task6/scripts/task6-1.js
+++ yaoyao/task6/scripts/task6-1.js
@@ -6,35 +6,47 @@ function $(elem_id){
 }
 var popup =  $("#popup");
 var popup2 = $("#popup2");
+var mask = $('#mask');
 var deltaX,deltaY;
 var scrollable = false;
 
-
+//禁用鼠标滚轮的按钮绑定点击事件
 $("#disable-scroll").addEventListener("click",function(e){
   scrollable = !scrollable;
   var txtScroll = scrollable === true ? "现在弹出后可以滚动，点击后禁用" : "现在弹出后禁用滚动，点击后开启";
   e.target.innerHTML = txtScroll;
 })
 
+// 通过全局事件委托，绑定点击事件
 document.addEventListener("click",function(e){
-  var target = e.target;
-  switch(target.id) {
+  switch(e.target.id) {
+    // 点击第一个浮层按钮，显示浮层1
     case "try" :
+      mask.style.display = "block";//这一句的顺序很关键，执行完 offsetHeight 刚好有值
       popup.style.display = "block";//offsetHeight为0，因为元素没有加载完，display还是none
-      $('#mask').style.display = "block";//这一句的顺序很关键，执行完 offsetHeight 刚好有值
+
+      // 这两句是为了让浮层在浏览器窗口居中显示
       popup.style.top = (window.innerHeight - popup.offsetHeight) / 2 + "px";
       popup.style.left = (window.innerWidth - popup.offsetWidth) / 2 + "px";
+
+      // 如果scrollable为false则绑定鼠标滚轮事件
       if(!scrollable) document.addEventListener("wheel", preventScroll)
       break;
 
+    // 点击第二个浮层按钮，显示浮层2
     case "try2" :
       popup2.style.display = "block";
-      $('#mask').style.display = "block";
+      mask.style.display = "block";
+
+      // 这两句是为了让浮层在浏览器窗口居中显示
       popup2.style.top = (window.innerHeight - popup2.offsetHeight) / 2 + "px";
       popup2.style.left = (window.innerWidth - popup2.offsetWidth) / 2 + "px";
+
+      // 如果scrollable为false则绑定鼠标滚轮事件
       if(!scrollable) document.addEventListener("wheel", preventScroll)
       break;
 
+    // 当点击蒙层、确定和取消按钮时，关闭浮层
     case "mask" :
     case "pop1-conf" :
     case "pop1-canc" :
@@ -43,48 +55,59 @@ document.addEventListener("click",function(e){
       popHidden();
       break;
 
+    // 第二个浮层里面的改变宽高的按钮
     case "pop2-change" :
       var popWidth = $("#pop-w").value;
       var popHeight = $("#pop-h").value;
-      if (popWidth >= 300 && popWidth<=800 && popHeight>=300 && popHeight<=800) {
+      if (popWidth >= 300 && popWidth<=800 && popHeight>=300 && popHeight<=600) {
+        // 获取输入框的值，更改浮层的宽高
         popup2.style.width = $("#pop-w").value + "px";
         popup2.style.height = $("#pop-h").value + "px";
+
+        // 这两句是为了让浮层在浏览器窗口居中显示
+        popup2.style.top = (window.innerHeight - popup2.offsetHeight) / 2 + "px";
+        popup2.style.left = (window.innerWidth - popup2.offsetWidth) / 2 + "px";
       }
       break;
    } 
 },false)
 
+// 取消表单默认提交事件
 $("#popup2").addEventListener("submit",function(e){
   e.preventDefault();
 })
 
+// 绑定鼠标点中事件，点中浮层标题栏，移动浮层
 $("#popup-top").addEventListener("mousedown", function(e){
   deltaX = popup.offsetLeft - e.clientX;
   deltaY = popup.offsetTop - e.clientY;
   popup.style.margin = 0;
 
-
-  document.addEventListener("mousemove", mousemove);
-  document.addEventListener("mouseup", mouseup);
+  document.addEventListener("mousemove", mouseMove);
+  document.addEventListener("mouseup", cancelMove);
 })
 
-function mousemove(e){
+// 浮层移动函数
+function mouseMove(e){
   popup.style.left = e.clientX + deltaX + "px";//向左边移就是减去变化的X值！往右边则相反
   popup.style.top = e.clientY + deltaY + "px";//向上边移就是减去变化的Y值！往下边则相反
 }
 
-function mouseup(){
-  document.removeEventListener("mousemove",mousemove)
-  document.removeEventListener("mouseup",mouseup)
+// 鼠标松开方法
+function cancelMove(){
+  document.removeEventListener("mousemove",mouseMove)
+  document.removeEventListener("mouseup",cancelMove)
 }
 
+// 鼠标滚轮失效函数
 function preventScroll(e){
   e.preventDefault();
 }
 
+// 关闭浮层函数
 function popHidden() {
   popup.style.display = "none";
   popup2.style.display = "none";
-  $('#mask').style.display = "none";
+  mask.style.display = "none";
   document.removeEventListener("wheel",preventScroll)
 }
